37578937ba41ccc98354f92153bfba6375a64cb7
Avnim21 <avnim2656@gmail.com>
2024-08-10 00:00:13 +0530

    Initializing the Game

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ for(i=1; i<=gridItems.length; i++)
         row = [];
     }
 }
+
+let row_index_1 = Math.floor(Math.random() * grid_col_size);
+let row_index_2 = Math.floor(Math.random() * grid_col_size);
+let col_index_1 = Math.floor(Math.random() * grid_row_size);
+let col_index_2 = Math.floor(Math.random() * grid_row_size);
+
+while(col_index_1 == col_index_2 && row_index_1 == row_index_2)
+{
+    let row_index_2 = Math.floor(Math.random() * grid_col_size);
+    let col_index_2 = Math.floor(Math.random() * grid_row_size);
+}
+
+matrix[row_index_1][col_index_1].firstElementChild.textContent = 2; 
+matrix[row_index_2][col_index_2].firstElementChild.textContent = 2;
