4b6b761ccb4fa4ba284b5df3c97a8679b4c19f12
Avnim21 <avnim2656@gmail.com>
2024-09-10 19:40:38 +0530

    Adding rules for Linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,30 @@ export default [
       }
     },
     rules: {
+
+    'sonarjs/no-duplicate-string': 'error',
+
+    'max-lines': ['error', 300],
+
+    'max-statements-per-line': ['error', { max: 1 }],
+
+    "no-magic-numbers": ["error", { "ignore": [0, 1], "ignoreArrayIndexes": true }],
+
+    "no-unused-vars": 'error',
+
+    "no-unreachable": "error",
     
+    "no-cond-assign": "warn",
+
+    "no-const-assign": "error",
+
+    "no-unreachable" : "error",
+
+    "no-use-before-define": "error",
+
+    'complexity': ['warn', { max: 10 }],
+
+    'sonarjs/no-small-switch': 'warn',
     },
   },
   pluginJs.configs.recommended,
