c7cf97bcd588d5d419e05b1075a0ffa951f5f1f8
Avnim21 <avnim2656@gmail.com>
2024-09-11 22:21:10 +0530

    Adding Unit Tests for getTileColor function

--- spec/Test.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Always passing test", function() {
-    it("should always pass", function() {
-        // This test will always pass because it just contains a passing expectation
-        expect(true).toBe(true);
-    });
-});
