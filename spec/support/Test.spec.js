380699c2fe3045cb999c0734d82844f66acd3902
Avnim21 <avnim2656@gmail.com>
2024-09-11 15:30:45 +0530

    Added Jasmine Packages

---
+++ spec/support/Test.spec.js
@@ -0,0 +1,6 @@
+describe("Always passing test", function() {
+    it("should always pass", function() {
+        // This test will always pass because it just contains a passing expectation
+        expect(true).toBe(true);
+    });
+});
